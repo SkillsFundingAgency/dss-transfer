151b4753548200f3233e8bbf8b763167dd667eb3
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-13 08:53:22 +0100

    Add Response Type to Post Method and updated Service to return new Guid

--- NCS.DSS.Transfer/PostTransferHttpTrigger/PostTransferHttpTrigger.cs
+++ NCS.DSS.Transfer/PostTransferHttpTrigger/PostTransferHttpTrigger.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -9,11 +11,23 @@ namespace NCS.DSS.Transfer.PostTransferHttpTrigger
     public static class PostTransferHttpTrigger
     {
         [FunctionName("Post")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/{interactionId:guid}/Transfers/")]HttpRequestMessage req, TraceWriter log)
+        [ResponseType(typeof(Models.Transfer))]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/{interactionId:guid}/Transfers/")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("Post Transfer C# HTTP trigger function processed a request.");
 
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            // Get request body
+            var transfer = await req.Content.ReadAsAsync<Models.Transfer>();
+
+            var transferService = new PostTransferHttpTriggerService();
+            var transferId = transferService.Create(transfer);
+
+            return transferId == null
+                ? new HttpResponseMessage(HttpStatusCode.BadRequest)
+                : new HttpResponseMessage(HttpStatusCode.Created)
+                {
+                    Content = new StringContent("Created Transfer record with Id of : " + transferId)
+                };
         }
     }
 }
--- NCS.DSS.Transfer/PostTransferHttpTrigger/PostTransferHttpTriggerService.cs
+++ NCS.DSS.Transfer/PostTransferHttpTrigger/PostTransferHttpTriggerService.cs
@@ -1,6 +1,15 @@
-﻿namespace NCS.DSS.Transfer.PostTransferHttpTrigger
+﻿using System;
+
+namespace NCS.DSS.Transfer.PostTransferHttpTrigger
 {
     public class PostTransferHttpTriggerService
     {
+        public Guid? Create(Models.Transfer transfer)
+        {
+            if (transfer == null)
+                return null;
+
+            return Guid.NewGuid();
+        }
     }
 }
