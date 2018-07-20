e5da02ee3e014a45fdfb206d48e8c50be298313d
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-20 11:06:49 +0100

    Add Http Helpers

---
+++ NCS.DSS.Transfer/Helpers/HttpRequestMessageHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NCS.DSS.Transfer.Helpers
+{
+    public class HttpRequestMessageHelper : IHttpRequestMessageHelper
+    {
+        public async Task<T> GetTransferFromRequest<T>(HttpRequestMessage req)
+        {
+            return await req.Content.ReadAsAsync<T>();
+        }
+    }
+}
