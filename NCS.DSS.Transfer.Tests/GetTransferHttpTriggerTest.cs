36b0c6f5ee96fb2eafc86fad972ef61e49f3c268
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-20 14:12:26 +0100

    Add Unit Tests

---
+++ NCS.DSS.Transfer.Tests/GetTransferHttpTriggerTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NCS.DSS.Transfer.Cosmos.Helper;
+using NCS.DSS.Transfer.GetTransferHttpTrigger.Service;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace NCS.DSS.Transfer.Tests
+{
+    [TestFixture]
+    public class GetTransferHttpTriggerTest
+    {
+        private const string ValidCustomerId = "7E467BDB-213F-407A-B86A-1954053D3C24";
+        private const string ValidInteractionId = "1e1a555c-9633-4e12-ab28-09ed60d51cb3";
+        private const string InValidId = "1111111-2222-3333-4444-555555555555";
+
+        private ILogger _log;
+        private HttpRequestMessage _request;
+        private IResourceHelper _resourceHelper;
+        private IGetTransferHttpTriggerService _getTransferHttpTriggerService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _request = new HttpRequestMessage()
+            {
+                Content = new StringContent(string.Empty),
+                RequestUri =
+                        new Uri($"http://localhost:7071/api/Customers/7E467BDB-213F-407A-B86A-1954053D3C24/" +
+                                $"Interactions/aa57e39e-4469-4c79-a9e9-9cb4ef410382/" +
+                                $"Transfer")
+            };
+            _log = Substitute.For<ILogger>();
+            _resourceHelper = Substitute.For<IResourceHelper>();
+            _getTransferHttpTriggerService = Substitute.For<IGetTransferHttpTriggerService>();
+        }
+
+        [Test]
+        public async Task GetTransferHttpTrigger_ReturnsStatusCodeBadRequest_WhenCustomerIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(InValidId, ValidInteractionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetTransferHttpTrigger_ReturnsStatusCodeBadRequest_WhenInteractionIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(ValidCustomerId, InValidId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetTransferHttpTrigger_ReturnsStatusCodeNoContent_WhenCustomerDoesNotExist()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(false);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetTransferHttpTrigger_ReturnsStatusCodeNoContent_WhenInteractionDoesNotExist()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(false);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+        
+        [Test]
+        public async Task GetTransferHttpTrigger_ReturnsStatusCodeNoContent_WhenTransferDoesNotExist()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(false);
+
+            _getTransferHttpTriggerService.GetTransfersAsync(Arg.Any<Guid>()).Returns(Task.FromResult<List<Models.Transfer>>(null).Result);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetTransferHttpTrigger_ReturnsStatusCodeOk_WhenTransferExists()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+
+            var listOfTransferes = new List<Models.Transfer>();
+            _getTransferHttpTriggerService.GetTransfersAsync(Arg.Any<Guid>()).Returns(Task.FromResult<List<Models.Transfer>>(listOfTransferes).Result);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        private async Task<HttpResponseMessage> RunFunction(string customerId, string interactionId)
+        {
+            return await GetTransferHttpTrigger.Function.GetTransferHttpTrigger.Run(
+                _request, _log, customerId, interactionId, _resourceHelper, _getTransferHttpTriggerService).ConfigureAwait(false);
+        }
+
+    }
+}
