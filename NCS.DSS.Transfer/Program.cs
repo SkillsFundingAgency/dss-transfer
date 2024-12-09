0c8ee077614868b4f60467bc587ec4bcbbd09805
hnandra <h_nandra@hotmail.co.uk>
2024-12-09 14:10:30 +0000

    Update Program.cs file to await HostBuilder
    Added application insights DI

--- NCS.DSS.Transfer/Program.cs
+++ NCS.DSS.Transfer/Program.cs
@@ -1,47 +1,83 @@
 using DFC.HTTP.Standard;
 using DFC.Swagger.Standard;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using NCS.DSS.Transfer.Cosmos.Helper;
 using NCS.DSS.Transfer.GetTransferByIdHttpTrigger.Service;
 using NCS.DSS.Transfer.GetTransferHttpTrigger.Service;
+using NCS.DSS.Transfer.Models;
 using NCS.DSS.Transfer.PatchTransferHttpTrigger.Service;
 using NCS.DSS.Transfer.PostTransferHttpTrigger.Service;
 using NCS.DSS.Transfer.Validation;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+namespace NCS.DSS.Transfer
+{
+    internal class Program
     {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-        services.AddLogging();
-        services.AddTransient<IGetTransferHttpTriggerService, GetTransferHttpTriggerService>();
-        services.AddTransient<IGetTransferByIdHttpTriggerService, GetTransferByIdHttpTriggerService>();
-        services.AddTransient<IPostTransferHttpTriggerService, PostTransferHttpTriggerService>();
-        services.AddTransient<IPatchTransferHttpTriggerService, PatchTransferHttpTriggerService>();
-        services.AddTransient<ISwaggerDocumentGenerator, SwaggerDocumentGenerator>();
-        services.AddTransient<IHttpRequestHelper, HttpRequestHelper>();
-        services.AddTransient<IResourceHelper, ResourceHelper>();
-        services.AddTransient<IValidate, Validate>();
-        services.AddTransient<IDynamicHelper, DynamicHelper>();
-    })
-    .ConfigureLogging(logging =>
-    {
-        // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
-        // For more information, see https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=windows#application-insights
-        logging.Services.Configure<LoggerFilterOptions>(options =>
+        private static async Task Main(string[] args)
         {
-            LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
-                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
-            if (defaultRule is not null)
-            {
-                options.Rules.Remove(defaultRule);
-            }
-        });
-    })
-    .Build();
+            var host = new HostBuilder()
+                .ConfigureFunctionsWebApplication()
+                .ConfigureServices((context, services) =>
+                {
+                    var configuration = context.Configuration;
+                    services.AddOptions<TransferConfigurationSettings>()
+                        .Bind(configuration);
 
-host.Run();
+                    services.AddApplicationInsightsTelemetryWorkerService();
+                    services.ConfigureFunctionsApplicationInsights();
+                    services.AddLogging();
+                    services.AddTransient<IGetTransferHttpTriggerService, GetTransferHttpTriggerService>();
+                    services.AddTransient<IGetTransferByIdHttpTriggerService, GetTransferByIdHttpTriggerService>();
+                    services.AddTransient<IPostTransferHttpTriggerService, PostTransferHttpTriggerService>();
+                    services.AddTransient<IPatchTransferHttpTriggerService, PatchTransferHttpTriggerService>();
+                    services.AddTransient<ISwaggerDocumentGenerator, SwaggerDocumentGenerator>();
+                    services.AddTransient<IHttpRequestHelper, HttpRequestHelper>();
+                    services.AddTransient<IResourceHelper, ResourceHelper>();
+                    services.AddTransient<IValidate, Validate>();
+                    services.AddTransient<IDynamicHelper, DynamicHelper>();
+
+                    services.AddSingleton(sp =>
+                    {
+                        var config = sp.GetRequiredService<IOptions<TransferConfigurationSettings>>().Value;
+                        config.TransferConnectionString = $"AccountEndpoint={config.Endpoint}/;AccountKey={config.Key};Database={config.CustomerDatabaseId};";
+
+                        var options = new CosmosClientOptions()
+                        {
+                            ConnectionMode = ConnectionMode.Gateway
+                        };
+
+                        return new CosmosClient(config.TransferConnectionString, options);
+                    });
+
+                    services.AddSingleton(sp =>
+                    {
+                        var config = sp.GetRequiredService<IOptions<TransferConfigurationSettings>>().Value;
+                        config.ServiceBusConnectionString =
+                            $"Endpoint={config.BaseAddress};SharedAccessKeyName={config.KeyName};SharedAccessKey={config.AccessKey}";
+
+                        return new Azure.Messaging.ServiceBus.ServiceBusClient(config.ServiceBusConnectionString);
+                    });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.Services.Configure<LoggerFilterOptions>(options =>
+                    {
+                        LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                            == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+                        if (defaultRule is not null)
+                        {
+                            options.Rules.Remove(defaultRule);
+                        }
+                    });
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
